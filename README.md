e41792f56472c733c71770d92854826aa9d03620
Belenkov <Vladislav.Belenkov@finbeat.tech>
2025-11-24 14:03:24 +0500

    add websocket service update readme

--- README.md
+++ README.md
@@ -1,46 +1,47 @@
 # obs_tracker
 
-## Описание
-Прототип приложения, которое 
-- подключается к Obs стриму,
-- делает скриншоты,позволяет рисовать тактическую графику для игроков,
-- показывает реплай до/после нескольких секунд от сделанного скриншота .
+## Review
+A prototype of an application that:
+
+- connects to the Obs stream,
+- takes screenshots, allows you to draw tactical graphics for players,
+- shows a replay before / after a few seconds from the screenshot taken.
 
 <img src="assets/preview.gif" alt="Demo" width="320" />
 
-## Установка
+## Install
 
 ### Obs studio
-1) Установите obs studio на ваш компьютер
-2) В tools -> "websocket server settings" включите "Enable Websocket server"
-3) Введите порт, задайте пароль
-4) В obs -> preferences -> output -> recording установите recording format: mp4
-5) В том же меню включите ReplyBuffer, задайте время реплая
-6) Задайте время реплая, например 60 секунд
-7) В obs -> preferences -> stream подключитесь к стриминговой платформе, например Twitch
-8) Запустите стрим, активируйте reply buffer в меню controls
+1) Install obs studio on your computer
+2) In tools -> "websocket server settings", enable "Enable Websocket server"
+3) Enter the port, enter the password
+4) In obs -> preferences -> output -> recording, set the recording format: mp4
+5) In the same menu, turn on ReplyBuffer, set the replay time
+6) Set the replay time, for example 60 seconds
+7) In obs -> preferences -> stream, connect to a streaming platform such as Twitch
+8) Start the stream, activate the reply buffer in the controls menu
 
-### Приложение
-1) Приложение должно быть в той же локальной сети, что и хост obs studio
-2) Скачайте последнюю актуальную версию кода, ветка main
-3) В obs_tacker/assets/config/app_config.json задайте 
-    - obsWebSocketUrl - урл и порт вебсокета. 
+### App
+1) The application must be on the same LAN as the obs studio host
+2) Download the latest current version of the code, main branch
+3) In obs_tacker/assets/config/app_config.set the json
+   - obsWebSocketUrl - the url and port of the websocket.
       - IOS Simulator/Destop/Web: ws://127.0.0.1:4455
       - Android Emulator : ws://10.0.2.2:4455
-      - Реальное устройство в одной сети с пк: ws://<локальный IP ПК>:4455
-    - obsWebSocketPassowrd - пароль из obs studio
-    - replyBufferDuration - длина повтора, должна быть такой же как в Obs studio
-    - preWindow - длительность записи до скриншота
-    - postWindow - длительность записи после скриншота
+      - A real device on the same network with a PC: ws://<local PC IP>:4455
+   - obsWebSocketPassowrd - password from obs studio
+   - replyBufferDuration - repeat length should be the same as in Obs studio
+   - preWindow - duration of recording before screenshot
+   - postWindow - duration of recording after screenshot
    
-4) Запустите проект:
+4) Launch project:
    - flutter pub get
    - flutter pub run build_runner build 
    - flutter run
 
 ### TODO
-- Реализовать сохранение данных между стримами, сейчас
-если предыдущий стрим закрывается, данные из него стираются
+- Implement data saving between streams, now
+  if the previous stream is closed, the data from it is erased
 
 
 
--- lib/feature/obs/data/obs_repository_impl.dart
+++ lib/feature/obs/data/obs_repository_impl.dart
@@ -4,23 +4,28 @@ import 'dart:io';
 import 'package:dartz/dartz.dart';
 import 'package:flutter/foundation.dart';
 import 'package:obs_tracker/core/failures/failures.dart';
+import 'package:obs_tracker/feature/obs/data/obs_websocket_datasource.dart';
 import 'package:obs_tracker/feature/obs/domain/obs_repository.dart';
 import 'package:obs_websocket/obs_websocket.dart';
 
 class ObsRepositoryImpl implements ObsRepository {
-  final ObsWebSocket obs;
+  final ObsWebsocketDatasource obs;
 
   ObsRepositoryImpl({required this.obs});
-
 
   @override
   Future<Either<Failure, Duration>> getCurrentRecordPosition() async {
     try {
-      final status = await obs.record.getRecordStatus();
+      final client = await obs.getClient();
 
-      return Right(Duration(milliseconds: status.outputDuration));
-    } on WebSocketException catch (e) {
-      return Left(ObsFailure(e.httpStatusCode, "Error of getting status", e));
+      return client.fold((failure) {
+        return Left(ObsFailure(-1, "Error of getting status", failure.cause));
+      }, (obs) async {
+        final status = await obs.record.getRecordStatus();
+        return Right(Duration(milliseconds: status.outputDuration));
+      });
+    } on SocketException catch (e) {
+      return Left(ObsFailure(e.port, "Error of getting status", e));
     } catch (e) {
       return Left(UnexpectedFailure("Unexpected error", e));
     }
@@ -29,33 +34,38 @@ class ObsRepositoryImpl implements ObsRepository {
   @override
   Future<Either<Failure, Uint8List>> takeScreenshot() async {
     try {
-      final currentSceneResp = await obs.sendRequest(
-        Request("GetCurrentProgramScene"),
-      );
+      final client = await obs.getClient();
+      return client.fold((failure) {
+        return Left(ObsFailure(-1, "Error of taking screenshot", failure.cause));
+      }, (obs) async {
+        final currentSceneResp = await obs.sendRequest(
+          Request("GetCurrentProgramScene"),
+        );
 
-      final response = await obs.sendRequest(
-        Request(
-          "GetSourceScreenshot",
-          requestData: {
-            "sourceName":
-                currentSceneResp?.responseData?["currentProgramSceneName"] ?? "default scene",
-            "imageFormat": "png",
-          },
-        ),
-      );
+        final response = await obs.sendRequest(
+          Request(
+            "GetSourceScreenshot",
+            requestData: {
+              "sourceName":
+                  currentSceneResp?.responseData?["currentProgramSceneName"] ?? "default scene",
+              "imageFormat": "png",
+            },
+          ),
+        );
 
-      var imageData = response?.responseData?["imageData"];
+        var imageData = response?.responseData?["imageData"];
 
-      if (imageData.startsWith('data:')) {
-        final commaIndex = imageData.indexOf(',');
-        if (commaIndex != -1) {
-          imageData = imageData.substring(commaIndex + 1);
+        if (imageData.startsWith('data:')) {
+          final commaIndex = imageData.indexOf(',');
+          if (commaIndex != -1) {
+            imageData = imageData.substring(commaIndex + 1);
+          }
         }
-      }
 
-      return Right(base64Decode(imageData));
-    } on WebSocketException catch (e) {
-      return Left(ObsFailure(e.httpStatusCode, "Error of taking screenshot", e));
+        return Right(base64Decode(imageData));
+      });
+    } on SocketException catch (e) {
+      return Left(ObsFailure(e.port, "Error of taking screenshot", e));
     } catch (e) {
       return Left(UnexpectedFailure("Unexpected error", e));
     }
@@ -64,14 +74,19 @@ class ObsRepositoryImpl implements ObsRepository {
   @override
   Future<Either<Failure, String>> saveReplayAndGetPath() async {
     try {
-      await obs.outputs.saveReplayBuffer("output");
+      final client = await obs.getClient();
+      return client.fold((failure) {
+        return Left(UnexpectedFailure("Unexpected error", failure.cause));
+      }, (obs) async {
+        await obs.outputs.saveReplayBuffer("output");
 
-      final resp = await obs.send("GetLastReplayBufferReplay");
-      final path = resp?.responseData?["savedReplayPath"] as String;
+        final resp = await obs.send("GetLastReplayBufferReplay");
+        final path = resp?.responseData?["savedReplayPath"] as String;
 
-      return Right(path);
-    } on WebSocketException catch (e) {
-      return Left(ObsFailure(e.httpStatusCode, "Error of saving reply", e));
+        return Right(path);
+      });
+    } on SocketException catch (e) {
+      return Left(ObsFailure(e.port, "Error of saving reply", e));
     } catch (e) {
       return Left(UnexpectedFailure("Unexpected error", e));
     }
